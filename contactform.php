5471d8df2e27c060905f786dff4cd185d4109fd9
celsoklososki <cklososki@hotmail.com>
2021-01-13 18:46:28 -0800

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -11,4 +11,5 @@ if(isset($_POST['submit'])){
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend")
 }
